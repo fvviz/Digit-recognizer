eb76d81720bce1e506f69f328137578cbdd906c1
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-10-18 22:20:43 -0600

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,6 +1,5 @@
 from app.GUI import GUI
 
-
-
-app = GUI(dim  = 200,model = "nn_model.model")
+app = GUI(dim=200, model="nn_model.model")
 app.init()
+app.mainloop()  # maybe move this call inside init function?
